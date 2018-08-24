48a6e8759b56f3d3e730e079c0b39657ab1fc99b
Aseda Sam <aoheneadu23@gmail.com>
2018-08-24 11:58:45 +0000

    Add assoc b/n user and article and add debugger view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,6 +16,7 @@ class ArticlesController < ApplicationController
   
   def create
     @article = Article.new(article_params)
+    @article.user = User.first #This is temporary. The user authentications will be done later.
     
     if @article.save
       flash[:success] = "Article was successfully created"
